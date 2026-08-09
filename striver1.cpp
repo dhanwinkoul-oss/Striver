5377e67804187532d8e1540583c3ebd399fdc0c3
dhanwinkoul-oss <dhanwinkaul@gmail.com>
2026-08-09 23:59:13 +0530

    created a new repo for dsa in cpp with the help of striver

---
+++ striver1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/*a school has following grading system take input from user 
+below 25 - F
+25 to 44 - E
+45 to 49 - D
+50 to 59 - C
+60 to 79 - B
+80 to 100 - A
+*/
+int main(){
+    int marks;
+    cin >> marks;
+
+
+
+    
+return 0;
+}
