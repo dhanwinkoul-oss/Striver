025a879706fb661dac93abe02ecb32b85da57160
dhanwinkoul-oss <dhanwinkaul@gmail.com>
2026-08-10 18:51:22 +0530

    made old code into comments and need to view what are the errors in main code

--- striver1.cpp
+++ striver1.cpp
@@ -8,12 +8,93 @@ below 25 - F
 60 to 79 - B
 80 to 100 - A
 */
+
+/*
 int main(){
     int marks;
     cin >> marks;
+if (marks < 25){
+    cout << "You have F grade\n";
+}
+else if (marks >= 25 && marks <= 44){
+    cout << "U have E grade\n";
+}
+else if (marks >= 45 && marks <= 49)
+{
+    cout << " U have D grade\n";
+}
 
+else if(marks >= 50 && marks <= 59)
+{
+    cout << " U have C grade\n";
+}
 
-
-    
+else if(marks >= 60 && marks <= 79)
+{
+    cout << " U have C grade\n";
+}
+else if(marks >= 80 && marks <= 100)
+{
+    cout << " U have C grade\n";
+}
+else {
+    cout << "enter a valid number from 1 to 100 \n";
+}
 return 0;
 }
+*/
+
+
+#include <iostream>
+using namespace std;
+
+/*
+A school has the following grading system:
+Below 25       - F
+25 to 44       - E
+45 to 49       - D
+50 to 59       - C
+60 to 79       - B
+80 to 100      - A
+*/
+
+int main()
+{
+    int marks;
+
+    cout << "Enter your marks: ";
+    cin >> marks;
+
+    if (marks < 0 || marks > 100)
+    {
+        cout << "Enter a valid number from 0 to 100\n";
+    }
+    else if (marks < 25)
+    {
+        cout << "You have F grade\n";
+    }
+    else if (marks <= 44)
+    {
+        cout << "You have E grade\n";
+    }
+    else if (marks <= 49)
+    {
+        cout << "You have D grade\n";
+    }
+    else if (marks <= 59)
+    {
+        cout << "You have C grade\n";
+    }
+    else if (marks <= 79)
+    {
+        cout << "You have B grade\n";
+    }
+    else
+    {
+        cout << "You have A grade\n";
+    }
+
+    return 0;
+}
+
+
