1bba56a120c17ccca66876bad8655f5f5f189407
dhanwinkoul-oss <dhanwinkaul@gmail.com>
2026-08-23 02:19:07 +0530

    some more concepts of pass by reference

--- passByReference.cpp
+++ passByReference.cpp
@@ -1,23 +1,96 @@
-// PASS BY REFERENCE FUNCTIONS 
+// // PASS BY REFERENCE FUNCTIONS 
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+
+//  // just by adding and symbol to the start it becomes pass by reference functions
+// void doSomething(string &s){
+//     s[0] = 't';
+//     cout << s << endl;
+// }
+
+// int main(){
+
+// string s = "raj";
+// doSomething(s);
+// cout << s << endl;
+
+
+
+
+// return 0;
+// }
+
+
+// more example 
+
 
 #include <bits/stdc++.h>
+
 using namespace std;
 
+void doSomething(int &num){
+cout << num << endl;
+num += 5;
+cout << num << endl;
+num += 5;
+cout << num << endl;
 
- // just by adding and symbol to the start it becomes pass by reference functions
-void doSomething(string &s){
-    s[0] = 't';
-    cout << s << endl;
+    
 }
 
 int main(){
+int num = 13;
+doSomething(num);
 
-string s = "raj";
-doSomething(s);
-cout << s << endl;
-
-
+cout << num << endl;
 
 
 return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
