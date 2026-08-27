7af9e9da3365293d83e5b6e00a6c5b03652e8ac2
dhanwinkoul-oss <dhanwinkaul@gmail.com>
2026-08-28 00:22:00 +0530

    made a code for 2 sum problem for leetcode but not fully functional

--- passByReference.cpp
+++ passByReference.cpp
@@ -93,61 +93,59 @@
 //  a better way to write this code 
 
 
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+
+// void doSomething(int arr[] , int n) {
+// arr [0] += 100;
+// cout << "value inside function :" << arr[0] << endl;
+
+// }
+
+
+// int main(){
+
+//     int n = 7;
+//     int arr [n];
+//     for (int i = 0; i <= n; i = i + 1){
+//     cin >> arr [i];
+// }
+
+
+//    doSomething(arr , n);
+//    cout << "value inside int main :" << arr[0] << endl;
+//    return 0;
+// }
+
+
+
 #include <bits/stdc++.h>
 
 using namespace std;
 
 
-void doSomething(int arr[] , int n) {
-arr [0] += 100;
-cout << "value inside function :" << arr[0] << endl;
+class solution {
+    public: 
+    vector<int> twoSum(vector<int> &nums, int target){
+        for (int i = 0; i < 4; i--){
+            cout << target;
+        }
+    }
+};
 
+
+int main (){
+
+    
+    
+ int target = 9;
+
+    for(int i = 0; i <= 4; i = i--){
+ cout << i << endl;
+ cin >> nums [i];
 }
-
-
-int main(){
-
-    int n = 7;
-    int arr [n];
-    for (int i = 0; i <= n; i = i + 1){
-    cin >> arr [i];
+    
+    return 0;
 }
-
-
-   doSomething(arr , n);
-   cout << "value inside int main :" << arr[0] << endl;
-   return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
