7ea9a7a9ddcbe023bc886da70ee94e66d0f587be
dhanwinkoul-oss <dhanwinkaul@gmail.com>
2026-08-28 18:14:04 +0530

    coded the star pattern for right angled triangle

--- patterns.cpp
+++ patterns.cpp
@@ -1,28 +1,100 @@
 
+// #include <iostream>
+// using namespace std;
+
+
+// // nested for loop 
+
+
+// int main (){
+
+
+// for(int i = 1; i <= 4; i ++){ // for loop for the rows 
+
+
+// for(int j = 1; j <= 5; j ++){ // this inner for loop for columns 
+
+//      cout << " * ";
+   
+// }
+
+// cout << endl;
+
+// }
+
+// return 0;
+// } 
+
+
+
 #include <iostream>
+
 using namespace std;
-
-
-// nested for loop 
-
 
 int main (){
 
+for(int i = 1; i <= 1; i++){
+cout << " *" << endl;
+for(int j = 2; j <= 2; j++){
+cout << " *" << " " << " *" << endl;
+    for(int k = 3; k <= 3; k++){
 
-for(int i = 1; i <= 4; i ++){ // for loop for the rows 
+cout << " *" << " " << " *" << " " << " *" << endl;
+        
+        
+    }
+  }
 
-
-for(int j = 1; j <= 5; j ++){ // this inner for loop for columns 
-
-     cout << " * ";
-   
+  
 }
-
-cout << endl;
-
-}
-
 
 
 return 0;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
