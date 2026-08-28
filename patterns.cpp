aebf885f9d71453fb68a4ab670e3525d77af7479
dhanwinkoul-oss <dhanwinkaul@gmail.com>
2026-08-28 17:58:16 +0530

    learnt square  star pattern using for loop

---
+++ patterns.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+using namespace std;
+
+
+// nested for loop 
+
+
+int main (){
+
+
+for(int i = 1; i <= 4; i ++){ // for loop for the rows 
+
+
+for(int j = 1; j <= 5; j ++){ // this inner for loop for columns 
+
+     cout << " * ";
+   
+}
+
+cout << endl;
+
+}
+
+
+
+return 0;
+}
