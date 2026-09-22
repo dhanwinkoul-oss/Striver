b274f2e1ae56b72448c11149dbff2a80182da799
dhanwinkoul-oss <dhanwinkaul@gmail.com>
2026-09-23 00:21:24 +0530

    coded for pattern 7 similarto reverse right angled star pattern but slightly different

--- patterns.cpp
+++ patterns.cpp
@@ -251,13 +251,44 @@
 // pattern 6 with test cases
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern6(int n){
+//     for (int i = 1; i<=n; i++){
+//         for(int j = 0; j < n - i + 1; j++){
+//             cout << "* ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// int main(){
+
+//     int t;
+//     cin >> t;
+//     for(int i=0;i <t; i++){
+//         int n;
+//         cin >> n;
+//         pattern6(n);
+//     }
+// }
+
+
+
+// pattern 7 
+
+
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-void pattern6(int n){
-    for (int i = 1; i<=n; i++){
-        for(int j = 0; j < n - i + 1; j++){
-            cout << "* ";
+void pattern7(int n){
+    for (int i = 0; i<=n; i++){
+        for(int j = 1; j < n - i + 1; j++){
+            cout << j;
         }
         cout << endl;
     }
@@ -271,11 +302,9 @@ int main(){
     for(int i=0;i <t; i++){
         int n;
         cin >> n;
-        pattern6(n);
+        pattern7(n);
     }
 }
-
-
 
 
 
