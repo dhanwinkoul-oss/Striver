01d7cba4cd2785958d077b5146b76c5a75020c42
dhanwinkoul-oss <dhanwinkaul@gmail.com>
2026-09-24 13:03:50 +0530

    coded for pattern 9 it was reverse of pattern 8

--- patterns.cpp
+++ patterns.cpp
@@ -312,21 +312,56 @@
 // pattern 8
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern8(int n){
+//     for (int i = 0; i<=n; i++){
+//         //space
+//         for(int j = 0; j < n - i + 1; j++){
+//             cout << " ";
+//         }
+//         //star
+//         for(int j = 0; j < 2*i+1; j++){
+//             cout << "*";
+//         }
+//         // space 
+//          for(int j = 0; j < n - i + 1; j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// int main(){
+
+//     int t;
+//     cin >> t;
+//     for(int i=0;i <t; i++){
+//         int n;
+//         cin >> n;
+//         pattern8(n);
+//     }
+// }
+
+// pattern 9
+ 
 #include <bits/stdc++.h>
 using namespace std;
 
 void pattern8(int n){
     for (int i = 0; i<=n; i++){
         //space
-        for(int j = 0; j < n - i + 1; j++){
+        for(int j = 0; j < i; j++){
             cout << " ";
         }
         //star
-        for(int j = 0; j < 2*i+1; j++){
+        for(int j = 0; j < 2 * n - (2*i+1); j++){
             cout << "*";
         }
         // space 
-         for(int j = 0; j < n - i + 1; j++){
+         for(int j = 0; j < i; j++){
             cout << " ";
         }
         cout << endl;
@@ -344,7 +379,6 @@ int main(){
         pattern8(n);
     }
 }
-
 
 
 
