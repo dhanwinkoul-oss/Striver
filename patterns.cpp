dfc2b0a24f40366a7619d917c3ab484e7c7e774c
dhanwinkoul-oss <dhanwinkaul@gmail.com>
2026-09-24 12:22:01 +0530

    coded the pattern 8

--- patterns.cpp
+++ patterns.cpp
@@ -282,13 +282,52 @@
 
 
 
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern7(int n){
+//     for (int i = 0; i<=n; i++){
+//         for(int j = 1; j < n - i + 1; j++){
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// int main(){
+
+//     int t;
+//     cin >> t;
+//     for(int i=0;i <t; i++){
+//         int n;
+//         cin >> n;
+//         pattern7(n);
+//     }
+// }
+
+
+
+
+// pattern 8
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
-void pattern7(int n){
+void pattern8(int n){
     for (int i = 0; i<=n; i++){
-        for(int j = 1; j < n - i + 1; j++){
-            cout << j;
+        //space
+        for(int j = 0; j < n - i + 1; j++){
+            cout << " ";
+        }
+        //star
+        for(int j = 0; j < 2*i+1; j++){
+            cout << "*";
+        }
+        // space 
+         for(int j = 0; j < n - i + 1; j++){
+            cout << " ";
         }
         cout << endl;
     }
@@ -302,7 +341,7 @@ int main(){
     for(int i=0;i <t; i++){
         int n;
         cin >> n;
-        pattern7(n);
+        pattern8(n);
     }
 }
 
